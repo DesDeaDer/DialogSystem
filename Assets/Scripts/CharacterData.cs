aa630c81697635aa3788a7c4402f0cb80ee8bf0b
__full_ing_arr <34687549+DesDeaDer@users.noreply.github.com>
2025-05-20 23:04:52 +0300

    Update CharacterData.cs

--- Assets/Scripts/CharacterData.cs
+++ Assets/Scripts/CharacterData.cs
@@ -2,35 +2,12 @@
 using UnityEngine;
 
 [Serializable]
-public class CharacterData
-{
-
+public class CharacterData {
     [SerializeField] private DialogCharacterID _characterID;
     [SerializeField] private TextID _nameID;
     [SerializeField] private SpeechCharacterStateID _stateID;
 
-    public DialogCharacterID CharacterID
-    {
-        get
-        {
-            return _characterID;
-        }
-    }
-
-    public SpeechCharacterStateID StateID
-    {
-        get
-        {
-            return _stateID;
-        }
-    }
-
-    public TextID NameID
-    {
-        get
-        {
-            return _nameID;
-        }
-    }
-
+    public DialogCharacterID CharacterID => _characterID;
+    public SpeechCharacterStateID StateID => _stateID;
+    public TextID NameID => _nameID;
 }
