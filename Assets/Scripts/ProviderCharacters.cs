b4153f655899e4c61d7a74594aed9ce99439ba54
__full_ing_arr <34687549+DesDeaDer@users.noreply.github.com>
2025-05-20 23:19:33 +0300

    Update ProviderCharacters.cs

--- Assets/Scripts/ProviderCharacters.cs
+++ Assets/Scripts/ProviderCharacters.cs
@@ -1,47 +1,20 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ProviderCharacters_", menuName = "Create/Data/Provider characters")]
-public class ProviderCharacters : ScriptableObject
-{
+public class ProviderCharacters : ScriptableObject {
     [SerializeField] private Data[] _data;
 
     [Serializable]
-    private struct Data
-    {
+    struct Data {
         [SerializeField] private DialogCharacterID _id;
         [SerializeField] private DialogCharacter _character;
 
-        public DialogCharacterID ID
-        {
-            get
-            {
-                return _id;
-            }
-        }
-
-        public DialogCharacter Character
-        {
-            get
-            {
-                return _character;
-            }
-        }
+        public DialogCharacterID ID => _id;
+        public DialogCharacter Character => _character;
     }
 
-    public DialogCharacter this[DialogCharacterID id]
-    {
-        get
-        {
-            foreach (var item in _data)
-            {
-                if (item.ID == id)
-                {
-                    return item.Character;
-                }
-            }
-
-            throw new ArgumentException();
-        }
-    }
+    public DialogCharacter this[DialogCharacterID id] //may be use more better mapping, for ex dictionary
+        => _data.First(d => d.ID == id);
 }
