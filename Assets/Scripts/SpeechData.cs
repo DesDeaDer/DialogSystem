6d32fba138ac9088d991448ee4f9af3c5398e30a
__full_ing_arr <34687549+DesDeaDer@users.noreply.github.com>
2025-05-20 23:20:26 +0300

    Update SpeechData.cs

--- Assets/Scripts/SpeechData.cs
+++ Assets/Scripts/SpeechData.cs
@@ -3,26 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class SpeechData
-{
-
+public class SpeechData {
     [SerializeField] private TextID _textID;
     [SerializeField] private CharacterData[] _characters;
 
-    public TextID TextID
-    {
-        get
-        {
-            return _textID;
-        }
-    }
-
-    public IEnumerable<CharacterData> Characters
-    {
-        get
-        {
-            return _characters;
-        }
-    }
-
+    public TextID TextID => _textID;
+    public IEnumerable<CharacterData> Characters => _characters;
 }
